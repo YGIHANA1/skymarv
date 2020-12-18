913137c4ca5f28d4e214ba887f526c465b8f5596
Yannick.Gi <61831175+YGIHANA1@users.noreply.github.com>
2020-12-18 13:39:53 +0100

    that

--- src/server.js
+++ src/server.js
@@ -23,6 +23,19 @@ const server = express()
 // const older_token= jwt.sign({
 //   data: {email:"password"}
 // }, 'secret', { expiresIn: '1h' });
+const whitelist=["http://localhost:3000","http://localhost:7000","https://git.heroku.com/marverixapp.git"]
+const corsOptions={
+  origin:function(callback,origin){
+    console.log("origin of request" + origin)
+if(whitelist.indexOf[origin] !== -1 || !origin){
+  console.log("origin acceptable")
+  callback(null,true)
+}else{
+    console.log("origin rejected")
+callback(new error("not allowed by CORS"))
+  }
+}
+  }
 
 const port = process.env.PORT || 7000
 const staticFolderPath = join(__dirname, ".../public")
@@ -30,7 +43,7 @@ server.use(express.static(staticFolderPath))
 server.use(express.json())
 if (process.env.NODE_ENV === "production")
 server.use(express.static("client/builds"))
-server.use(cors())
+server.use(cors(corsOptions))
 
 server.use("/users", usersRouter)
 server.use("/comics", comicsRouter)
